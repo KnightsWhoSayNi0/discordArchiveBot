e359cd5e3137b942e63a0681101d799d39fc4a2f
KnightsWhoSayNi <62893792+KnightsWhoSayNi0@users.noreply.github.com>
2021-11-01 11:45:29 -0400

    Create Context.java
    hope this works

---
+++ src/main/java/me/knightswhosayni/bot/commands/impl/Context.java
@@ -0,0 +1,41 @@
+package me.knightswhosayni.bot.commands.impl;
+
+import me.knightswhosayni.archive.Librarian;
+import me.knightswhosayni.archive.Message;
+import me.knightswhosayni.bot.Bot;
+import me.knightswhosayni.bot.commands.Command;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+
+import java.util.List;
+
+public class Context extends Command {
+
+    public Context() {
+        super("context", "Displays the next quote in sequence, after the last random.", "context", new String[]{"c"});
+    }
+
+    @Override
+    public void onCommand(String[] args, MessageReceivedEvent e) {
+        if (args.length == 1) {
+            Message m = Librarian.getLastMessage();
+
+            EmbedBuilder embed = new EmbedBuilder()
+                    .setDescription(m.getContent())
+                    .setAuthor(m.getAuthor())
+                    .setTimestamp(m.getDate());
+
+            e.getChannel().sendMessage(embed.build()).queue();
+
+            if (!m.getAttachments().isEmpty()) {
+                e.getChannel().sendMessage(m.getAttachments()).queue();
+            }
+        } else {
+          e.getChannel().sendMessage(new EmbedBuilder()
+                  .setTitle("Incorrect syntax.")
+                  .setDescription("Syntax is: " + this.getSyntax())
+                  .setColor(0xff22ff77)
+                  .build()).queue();
+        }
+    }
+}
